59f13b546558ed2462b65bbc400bce1af9b4b0d9
raviranjan81 <raviranjan8023010@gmail.com>
2024-04-26 13:02:36 +0530

    login correct

--- lib/Pages/Login.dart
+++ lib/Pages/Login.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:tutorial/Pages/Home.dart';
 
 class Login extends StatelessWidget {
@@ -7,65 +6,67 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      // color: Colors.deepOrange.shade100,
-      child: Center(
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/img.png",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Welcome',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600),
-            ),
-            Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                          hintText: 'Enter user name',
-                          labelText: "User name",
+    return SingleChildScrollView(
+      child: Material(
+        // color: Colors.deepOrange.shade100,
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/img.png",
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Welcome',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600),
+              ),
+              Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Enter user name',
+                            labelText: "User name",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                       ),
+                       SizedBox(height: 20,),
+                       TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Enter your password',
+                          labelText: 'Password',
                           border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10))),
-                     ),
-                     SizedBox(height: 20,),
-                     TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: 'Enter your password',
-                        labelText: 'Password',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)
+                            borderRadius: BorderRadius.circular(10)
+                          ),
+                        ),
+                       ),
+                       SizedBox(height: 20,),
+                     ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Home()),
+                          );
+                        },
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                     ),
-                     SizedBox(height: 20,),
-                   ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Home()),
-                        );
-                      },
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                ))
-          ],
+                    ],
+                  ))
+            ],
+          ),
         ),
       ),
     );
